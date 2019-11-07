6b7d115f7d27258607b4060121ea99477915fbde
GroovyMikee <mkupisz9619@gmail.com>
2019-11-07 15:53:35 +0100

    new view of app

--- script.js
+++ script.js
@@ -1,84 +1,49 @@
-function gen() {
-    const calcScreen = document.getElementById("screen")
-    const cDelete = document.getElementById("cDelete")
-    const main = document.getElementById("main")
-    const main2 = document.getElementById("main2")
-    const main3 = document.getElementById("main3")
-    const main4 = document.getElementById("main4")
-
-    const screen = document.createElement("input")
-    screen.setAttribute("type", "text")
-    screen.setAttribute("value", "")
-    screen.setAttribute("disabled", "true")
-    calcScreen.appendChild(screen)
-
-    for (let i = 0; i < 10; i++) {
-        const keyi = document.createElement("button")
-        keyi.innerText = i
-        keyi.onclick = show
-        keyi.dataset.value = i
-        if(i === 0) {
-            main4.appendChild(keyi)
-        } else if (i > 0 && i <= 3) {
-            main3.appendChild(keyi)
-        } else if (i > 3 && i < 7) {
-            main2.appendChild(keyi)
-        } else {
-            main.appendChild(keyi)
-        }
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
     }
 
-    const keyC = document.createElement("button")
-    keyC.innerText = "C"
-    keyC.dataset.value = "C"
-    keyC.onclick = show
-    cDelete.appendChild(keyC)
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.calculation = undefined
+    }
 
-    const keyAC = document.createElement("button")
-    keyAC.innerText = "AC"
-    keyAC.dataset.value = "AC"
-    keyAC.setAttribute("id", "AC")
-    keyAC.onclick = show
-    cDelete.appendChild(keyAC)
+    delete() {
 
-    const keyDot = document.createElement("button")
-    keyDot.innerText = "."
-    keyDot.dataset.value = "."
-    keyDot.onclick = show
-    main4.appendChild(keyDot)
+    }
 
-    const keyEq = document.createElement("button")
-    keyEq.innerText = "="
-    keyEq.dataset.value = "="
-    keyEq.onclick = show
-    main4.appendChild(keyEq)
+    appendNumber(number) {
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
 
-    const keyAdd = document.createElement("button")
-    keyAdd.innerText = "+"
-    keyAdd.dataset.value = "+"
-    keyAdd.onclick = show
-    main4.appendChild(keyAdd)
+    chooseOperation(operation) {
 
-    const keyMin = document.createElement("button")
-    keyMin.innerText = "-"
-    keyMin.dataset.value = "-"
-    keyMin.onclick = show
-    main3.appendChild(keyMin)
+    }
 
-    const keyDiv = document.createElement("button")
-    keyDiv.innerText = "/"
-    keyDiv.dataset.value = "/"
-    keyDiv.onclick = show
-    main2.appendChild(keyDiv)
+    compute() {
 
-    const keyMult = document.createElement("button")
-    keyMult.innerText = "*"
-    keyMult.dataset.value = "*"
-    keyMult.onclick = show
-    main.appendChild(keyMult)
+    }
+
+    updateScreen() {
+        this.currentOperandTextElement.innerText = this.currentOperand
+    }
 }
 
-function show() {
-    console.log(this.dataset.value)
-}
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateScreen()
+    })
+})
